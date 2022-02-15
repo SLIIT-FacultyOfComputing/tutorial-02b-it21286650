da6029d250603209ded3c6f88fee70935a452e0b
it21286650 <99391707+it21286650@users.noreply.github.com>
2022-02-15 05:56:16 +0000

    I have done tutorial 1,2,3 and 4

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,16 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  int x,answer=1;
+  for (x=1;x<=no;x++)
+  {
+    answer=answer*x;
+  }
+  return answer;
+}  
+long nCr(int n, int r)
+{
+  return (Factorial(n)/(Factorial(r)*Factorial(n-r)));
+}
